40d7cc51c3261525b7b707b86828afb0255cdf1b
Helm20 <67297359+Helm20@users.noreply.github.com>
2020-06-23 01:25:50 +0100

    Le but de ce projet est de créer un jeu en utilisant Unity.

---
+++ Roll a ball/Assets/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+    private Rigidbody rb;
+    private int count;
+    public float speed;
+    public float cris;
+    public Text CountText;
+    public Text GetAll;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+        count = 0;
+        Count();
+        GetAll.text = "";
+    }
+
+    // Update is called once per frame
+
+     void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Mouse X");
+        float moveV = Input.GetAxis("Mouse Y");
+        Console.WriteLine(moveH);
+        Vector3 f = new Vector3(moveH, 0, moveV);
+        rb.AddForce(f*speed*cris);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            Count();
+        }
+
+    }
+    void Count()
+    {
+        CountText.text = " Count : " + count.ToString();
+        if (count == 19) {
+            GetAll.text = " You got them all!";
+        
+        }
+    }
+
+}
